1761aa0dfa205657969e9f176b5e572241dc9347
HARSH SINGH <harshsingh.bar@gmail.com>
2024-09-26 17:36:01 +0530

    resetting db link

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI2)
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
